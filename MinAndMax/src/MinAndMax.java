ae28155aa3fb97174c4fe1dd5e6aa8de24e18eb1
Zain Haroon <zainharoonyahya@gmail.com>
2025-06-04 18:20:58 +0500

    Updated MinAndMax class

--- MinAndMax/src/MinAndMax.java
+++ MinAndMax/src/MinAndMax.java
@@ -1,25 +1,43 @@
 import java.util.Scanner;
 
 public class MinAndMax {
+    static double prevMax = 0;
+    static double prevMin = 0;
     public static void main(String[] args) {
-        minAndMax();
+        System.out.println(minAndMax());
     }
 
     public static String minAndMax(){
         Scanner scanner = new Scanner(System.in);
 
-        boolean isNumber = true;
+        double maxValue = 0;
+        double minValue = 0;
 
-        while (isNumber) {
-            System.out.println("Enter a number, or any character to quit");
-            try {
-                double value = Double.parseDouble(scanner.nextLine());
-            } catch (NumberFormatException characterData) {
-                isNumber = false;
-            }
-
+        while (true){
+                try {
+                    System.out.println("Enter a number, or any character to quit");
+                    String input = scanner.nextLine();
+                    maxValue = Maximum(Double.parseDouble(input));
+                    minValue = Minimum(Double.parseDouble(input));
+                } catch (NumberFormatException characterData) {
+                    break;
+                }
         }
-        return "";
+        return "Max: " + maxValue + ", Min: " + minValue;
+    }
+
+    public static double Maximum(double number){
+        if (number > prevMax){
+            prevMax = number;
+        }
+        return prevMax;
+    }
+
+    public static double Minimum(double number){
+        if (number < prevMin){
+            prevMin = number;
+        }
+        return prevMin;
     }
 
 }
