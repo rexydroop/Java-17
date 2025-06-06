4ddcf7de9fd10532c5ad6215c15fd0f5c634ccb5
Zain Haroon <zainharoonyahya@gmail.com>
2025-06-06 23:20:53 +0500

    Added getBucketCount method to PaintJob class

--- PaintJob/src/PaintJob.java
+++ PaintJob/src/PaintJob.java
@@ -1,2 +1,24 @@
 public class PaintJob {
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(3.4, 2.3, 2, 1));
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+            return -1;
+        }
+
+        double area = width * height;
+        int buckets = 0;
+        int totalBuckets = extraBuckets + buckets;
+        double totalPaintAvailable = areaPerBucket * totalBuckets;
+
+        while (totalPaintAvailable < area){
+            buckets++;
+            totalBuckets = extraBuckets + buckets;
+            totalPaintAvailable = areaPerBucket * totalBuckets;
+        }
+        return buckets;
+    }
 }
