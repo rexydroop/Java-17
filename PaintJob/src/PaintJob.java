523e9a4bdf7a35f546fa3d5fc3f9a5664be619cf
Zain Haroon <zainharoonyahya@gmail.com>
2025-06-07 03:44:24 +0500

    Updated PaintJob class

--- PaintJob/src/PaintJob.java
+++ PaintJob/src/PaintJob.java
@@ -1,24 +1,38 @@
 public class PaintJob {
     public static void main(String[] args) {
-        System.out.println(getBucketCount(3.4, 2.3, 2, 1));
+        System.out.println(getBucketCount(3.26, 0.75));
     }
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
 
-    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
-
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
             return -1;
         }
 
         double area = width * height;
-        int buckets = 0;
-        int totalBuckets = extraBuckets + buckets;
-        double totalPaintAvailable = areaPerBucket * totalBuckets;
+        double areaLeft = area - areaPerBucket * extraBuckets;
 
-        while (totalPaintAvailable < area){
-            buckets++;
-            totalBuckets = extraBuckets + buckets;
-            totalPaintAvailable = areaPerBucket * totalBuckets;
+        return getBucketCount(areaLeft, areaPerBucket);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1
+    ;
         }
-        return buckets;
+
+        double area = width * height;
+
+        return getBucketCount(area, areaPerBucket);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+
+
+        double buckets = area / areaPerBucket;
+
+        return (int) Math.ceil(buckets);
     }
 }
