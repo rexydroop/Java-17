33ae506732a8eade874d426a7bf36b5c50d46835
Zain Haroon <zainharoonyahya@gmail.com>
2025-06-14 01:01:44 +0500

    Updated TaskTracker class

--- TaskTracker/src/TaskTracker.java
+++ TaskTracker/src/TaskTracker.java
@@ -1,9 +1,15 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class TaskTracker {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_GREEN = "\u001B[92m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
         taskTracker(); //Call method
     }
@@ -14,18 +20,24 @@ public class TaskTracker {
 
         System.out.println("\n--- Task Tracker ---\n");
         System.out.println("Type 'exit' to quit");
-        System.out.println("Type 'clear' to delete all previous tasks\n");
+        System.out.println("Type 'clear' to delete all previous tasks");
+        System.out.println("Type 'tasks' to display all previous tasks\n");
 
         while (true) {
             System.out.print("Task: ");
             String task = scanner.nextLine();
 
             if (task.equalsIgnoreCase("exit")) {
-                System.out.println("Goodbye"); //Print Goodbye
+                System.out.println("- " + ANSI_GREEN + "Goodbye" + ANSI_RESET + " -"); //Print Goodbye
                 break; //Exit loop
             }
             if (task.equalsIgnoreCase("clear")) {
                 clearFile(file); //Clear file
+                System.out.println("--- " + ANSI_RED + "All tasks have been cleared!" + ANSI_RESET);
+                continue; //Restart
+            }
+            if (task.equalsIgnoreCase("tasks")) {
+                printFile(file); //print all tasks in the file
                 continue; //Restart
             }
 
@@ -34,7 +46,6 @@ public class TaskTracker {
             System.out.println();
 
             try {
-
                 file.createNewFile(); //Creates new file
                 FileWriter writer = new FileWriter(file, true); //Create an instance of the FileWriter class
 
@@ -55,5 +66,24 @@ public class TaskTracker {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void printFile(File file){
+        try {
+            Scanner reader = new Scanner(file);
+            if (!reader.hasNextLine()){
+                System.out.println("--- " + ANSI_RED + "No tasks have been saved!" + ANSI_RESET);
+            }
+            int taskNumber = 1;
+            while (reader.hasNextLine()){
+                System.out.println("--- Task #" + taskNumber + ": " + ANSI_BLUE + reader.nextLine() + ANSI_RESET);
+                taskNumber++;
+            }
+        }catch (FileNotFoundException notFoundException){
+            System.out.println("--- " + ANSI_RED + "File was not found!" + ANSI_RESET);
+            notFoundException.printStackTrace();
+        }
+
+
     }
 }
